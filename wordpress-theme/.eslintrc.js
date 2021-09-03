d6b5d180a0f72bbb2ed6f84c45139cef2a62d1ad
Armand Philippot <git@armandphilippot.com>
2021-09-03 12:06:39 +0200

    chore: update ESlint, Prettier, Stylelint, Babel and PHPCS config

--- wordpress-theme/.eslintrc.js
+++ wordpress-theme/.eslintrc.js
@@ -1,9 +1,21 @@
 module.exports = {
+	root: true,
 	env: {
 		browser: true,
 		commonjs: true,
 		es6: true,
 		node: true,
 	},
-	extends: ['plugin:@wordpress/eslint-plugin/recommended'],
+	extends: [ 'plugin:@wordpress/eslint-plugin/recommended-with-formatting' ],
+	rules: {
+		'@wordpress/no-unused-vars-before-return': 'error',
+	},
+	overrides: [
+		{
+			files: [ './tools/**' ],
+			rules: {
+				'no-console': 'off',
+			},
+		},
+	],
 };
